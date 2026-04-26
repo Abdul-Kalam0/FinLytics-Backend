cba783f1d9b4a4d7cd4566533eeffd0f6166fcac
Abdul-Kalam0 <abdulkalamblycomp@gmail.com>
2026-04-26 17:53:26 +0530

    Updated the config of logout

--- controllers/authController.js
+++ controllers/authController.js
@@ -142,8 +142,8 @@ export const logout = async (req, res) => {
   try {
     res.clearCookie("token", {
       httpOnly: true,
-      secure: false,
-      sameSite: "strict",
+      secure: true,
+      sameSite: "none",
     });
     return res.status(200).json({
       success: true,
